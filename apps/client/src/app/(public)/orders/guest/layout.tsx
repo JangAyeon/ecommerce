f1c2ba39a2d0f30be51d1e718768037c6111a59c
JangAyeon <ayeon3052@ewhain.net>
2025-12-21 14:38:33 +0900

    refactor(web): layout props 중복되는 것들 정리

---
+++ apps/client/src/app/(public)/orders/guest/layout.tsx
@@ -0,0 +1,7 @@
+import { LayoutProps } from "@/@types/UI/layout";
+
+const GuestOrderLookupLayout = ({ children }: LayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default GuestOrderLookupLayout;
